fd604d28c4b2f823ee1ebf97484d8faedd505c11
sps03 <shantanusonwane20@cse.iiitp.ac.in>
2024-04-10 16:23:35 +0530

    Cryptosm change

--- src/app/components/Crypto.tsx
+++ src/app/components/Crypto.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Crypto = () => {
   return (
-    <div className='whitespace-nowrap w-[350px] sm:w-full overflow-x-auto scrollbar-hidden sm:flex flex gap-3' style={{ transition: 'transform 0.3s ease' }}>
-      <div className="flex gap-3 items-center  py-1.5 pl-3.5 ml-8 justify-start text-sm w-full sm:w-[240px] text-white bg-zinc-900 rounded-[160px]">
+    <div className='whitespace-nowrap w-[350px] sm:w-full overflow-x-auto scrollbar-hidden sm:flex flex gap-3'>
+      <div className="flex gap-3 items-center px-10 py-1.5 sm:py-1.5 pl-3.5 ml-8 justify-start text-xs sm:text-sm w-full sm:w-[240px] text-white bg-zinc-900 rounded-[160px]">
         <div className="self-stretch my-auto">COC</div>
         <div className="self-stretch my-auto">$ 9.00.000</div>
         <div className="flex gap-1 self-stretch text-green-400 whitespace-nowrap">
@@ -16,7 +16,20 @@ const Crypto = () => {
         </div>
       </div>
 
-      <div className="flex gap-3 items-center px-3.5 py-1.5 justify-start w-full sm:w-[240px] text-sm text-white bg-zinc-900 rounded-[160px]">
+      <div className="flex gap-3 items-center px-10 py-1.5 sm:py-1.5 pl-3.5  justify-start text-xs sm:text-sm w-full sm:w-[240px] text-white bg-zinc-900 rounded-[160px]">
+        <div className="self-stretch my-auto">COC</div>
+        <div className="self-stretch my-auto">$ 9.00.000</div>
+        <div className="flex gap-1 self-stretch text-red-500 whitespace-nowrap">
+        <img
+            loading="lazy"
+            src="https://cdn.builder.io/api/v1/image/assets/TEMP/b04defe118c37ddc155d0cee00f39d82e7681c3fc9339d45dfe71b5d923e97a4?apiKey=fc314cf36d364dbdb6a0cce0fe9d0082&"
+            className="shrink-0 w-6 aspect-square"
+          />
+          <div className="my-auto">114.23%</div>
+        </div>
+      </div>
+
+      {/* <div className="flex gap-3 text-left sm:px-3.5 sm:py-1.5 px-8 py-1.5 justify-start w-full sm:w-[240px] text-xs sm:text-sm text-white bg-zinc-900 rounded-[160px]">
         <div className="self-stretch my-auto">COC</div>
         <div className="self-stretch my-auto">$ 9.00.000</div>
         <div className="flex gap-1 self-stretch text-red-500 whitespace-nowrap">
@@ -27,9 +40,22 @@ const Crypto = () => {
           />
           <div className="my-auto">114.23%</div>
         </div>
+      </div> */}
+
+      <div className="flex gap-3 items-center px-10 py-1.5 sm:py-1.5 pl-3.5  justify-start text-xs sm:text-sm w-full sm:w-[240px] text-white bg-zinc-900 rounded-[160px]">
+        <div className="self-stretch my-auto">COC</div>
+        <div className="self-stretch my-auto">$ 9.00.000</div>
+        <div className="flex gap-1 self-stretch text-purple-600 whitespace-nowrap">
+        <img
+            loading="lazy"
+            src="https://cdn.builder.io/api/v1/image/assets/TEMP/a1ad2432f769406908b0d92d71fff7b272838526a043342f34b973f374e6ac8a?apiKey=fc314cf36d364dbdb6a0cce0fe9d0082&"
+            className="shrink-0 w-6 aspect-square"
+          />
+          <div className="my-auto">114.23%</div>
+        </div>
       </div>
 
-      <div className="flex gap-3 items-center px-3.5 py-1.5 justify-start w-full sm:w-[240px] text-sm text-white bg-zinc-900 rounded-[160px]">
+      {/* <div className="flex gap-3 items-center sm:px-3.5 sm:py-1.5 text-xs justify-start w-full sm:w-[240px] px-8 py-1.5 sm:text-sm text-white bg-zinc-900 rounded-[160px]">
         <div className="self-stretch my-auto">COC</div>
         <div className="self-stretch my-auto">$ 9.00.000</div>
         <div className="flex gap-1 self-stretch text-purple-600 whitespace-nowrap">
@@ -40,9 +66,22 @@ const Crypto = () => {
           />
           <div className="my-auto">114.23%</div>
         </div>
+      </div> */}
+
+      <div className="flex gap-3 items-center px-10 py-1.5 sm:py-1.5 pl-3.5  justify-start text-xs sm:text-sm w-full sm:w-[240px] text-white bg-zinc-900 rounded-[160px]">
+        <div className="self-stretch my-auto">COC</div>
+        <div className="self-stretch my-auto">$ 9.00.000</div>
+        <div className="flex gap-1 self-stretch text-red-500 whitespace-nowrap">
+        <img
+            loading="lazy"
+            src="https://cdn.builder.io/api/v1/image/assets/TEMP/b04defe118c37ddc155d0cee00f39d82e7681c3fc9339d45dfe71b5d923e97a4?apiKey=fc314cf36d364dbdb6a0cce0fe9d0082&"
+            className="shrink-0 w-6 aspect-square"
+          />
+          <div className="my-auto">114.23%</div>
+        </div>
       </div>
 
-      <div className="flex gap-3 items-center px-3.5 py-1.5justify-start w-full sm:w-[240px] text-sm text-white bg-zinc-900 rounded-[160px]">
+      {/* <div className="flex gap-3 items-centersm:px-3.5 sm:py-1.5 text-xs justify-start w-full sm:w-[240px] px-8 py-1.5 sm:text-sm text-white bg-zinc-900 rounded-[160px]">
         <div className="self-stretch my-auto">COC</div>
         <div className="self-stretch my-auto">$ 9.00.000</div>
         <div className="flex gap-1 self-stretch text-red-500 whitespace-nowrap">
@@ -53,9 +92,22 @@ const Crypto = () => {
           />
           <div className="my-auto">114.23%</div>
         </div>
+      </div> */}
+
+      <div className="flex gap-3 items-center px-10 py-1.5 sm:py-1.5 pl-3.5  justify-start text-xs sm:text-sm w-full sm:w-[240px] text-white bg-zinc-900 rounded-[160px]">
+        <div className="self-stretch my-auto">COC</div>
+        <div className="self-stretch my-auto">$ 9.00.000</div>
+        <div className="flex gap-1 self-stretch text-green-400 whitespace-nowrap">
+        <img
+            loading="lazy"
+            src="https://cdn.builder.io/api/v1/image/assets/TEMP/a1ad2432f769406908b0d92d71fff7b272838526a043342f34b973f374e6ac8a?apiKey=fc314cf36d364dbdb6a0cce0fe9d0082&"
+            className="shrink-0 w-6 aspect-square"
+          />
+          <div className="my-auto">114.23%</div>
+        </div>
       </div>
 
-      <div className="flex gap-3 items-center px-3.5 py-1.5 justify-start w-full sm:w-[240px] text-sm text-white bg-zinc-900 rounded-[160px]">
+      {/* <div className="flex gap-3 items-center sm:px-3.5 sm:py-1.5 text-xs justify-start w-full sm:w-[240px] px-8 py-1.5 sm:text-sm text-white bg-zinc-900 rounded-[160px]">
         <div className="self-stretch my-auto">COC</div>
         <div className="self-stretch my-auto">$ 9.00.000</div>
         <div className="flex gap-1 self-stretch text-green-400 whitespace-nowrap">
@@ -66,7 +118,7 @@ const Crypto = () => {
           />
           <div className="my-auto">114.23%</div>
         </div>
-      </div>
+      </div> */}
     </div>
   )
 }
